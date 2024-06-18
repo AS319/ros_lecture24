80d07452ed9e30033e345751bf368b32302bdec0
AS319 <aichitead24401@gmail.com>
2024-06-18 15:45:18 +0900

    commit

--- src/kadai03.py
+++ src/kadai03.py
@@ -1,4 +1,4 @@
-gpukei=0
+goukei=0
 for i in range(1,11):
   goukei+=i
 print(goukei)
---
+++ src/kadai04.py
@@ -0,0 +1,6 @@
+def greet(name):
+  print("Hello "+str(name))
+name="AAA"
+greet(name)
+name="BBB"
+greet(name)
